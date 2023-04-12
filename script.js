422f769e5c9b66e4481ea620e5822fd4bfbbbaab
sureshvaddegithub <116425651+sureshvaddegithub@users.noreply.github.com>
2023-04-12 11:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function threeSum(arr, target) {
 // write your code here
-  
+
+	arr.sort((a,b) =>{
+		return a-b;
+	})
+
+	let ans = Number.MAX_VALUE;
+
+	for(let i = 0;i<arr.length-2;i++){
+		let j = i+1;
+		let k = arr.length-1;
+		while(j<k){
+			let sum = arr[i]+arr[j]+arr[k];
+			if(sum == target){
+				return target;
+			}
+			if(sum<target){
+				if(ans == Number.MAX_VALUE)
+					ans = sum;
+				if(Math.abs(ans-target)>Math.abs(sum-target))
+					ans = sum;
+				j++;
+			}
+			else{
+				if(ans == Number.MAX_VALUE)
+					ans = sum;
+				if(Math.abs(ans-target)>Math.abs(sum-target))
+					ans = sum;
+				k--;
+			}
+		}
+	}
+  return ans;
 }
 
 module.exports = threeSum;
